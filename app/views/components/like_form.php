0dea3519896dcfd12863d95374f594e408419c18
220003 <220003pb@yse-c.net>
2023-11-10 14:39:27 +0900

    f

--- app/views/components/like_form.php
+++ app/views/components/like_form.php
@@ -7,11 +7,9 @@
         <?php if (in_array($tweet['id'], $user_likes)) : ?>
             <!-- ユーザーがいいねをしている場合 -->
             <button type="submit" class="btn btn-sm"><img src="../images/svg/heart_active.svg"></button>
-            <span class="like-count"><?= @$like_counts[$tweet['id']] ?></span>
         <?php else : ?>
             <!-- ユーザーがいいねをしていない場合 -->
             <button type="submit" class="btn btn-sm"><img src="../images/svg/heart.svg"></button>
-            <span class="like-count"><?= @$like_counts[$tweet['id']] ?></span>
         <?php endif ?>
     </form>
 </div>
